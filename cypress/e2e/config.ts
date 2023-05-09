b68bf29dee16048bd921f298d461a2dd7e38a283
SindhuThalla <83402653+SindhuThalla@users.noreply.github.com>
2023-05-09 23:31:57 +0100

    Add files via upload

---
+++ cypress/e2e/config.ts
@@ -0,0 +1,3 @@
+export const DONATION_URL = "https://app.pws.int.cruk.org/support-us/your-donation"
+export const DETAILS_URL = "https://donate.cancerresearchuk.org/support-us/details"
+export const PAYMENT_URL ="https://donate.cancerresearchuk.org/support-us/payment"
